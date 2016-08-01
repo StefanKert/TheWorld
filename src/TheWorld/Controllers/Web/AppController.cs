ce8416df4719e3a4e32988d70042691c3d264bee
Stefan Kert <stefan.kert@inode.at>
2016-08-01 19:05:28 +0200

    t

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +28,19 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+                ModelState.AddModelError("", "We don´t support AOL addresses");
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From theWorld", model.Message);
+                ViewBag.UserMessage = "Message sent.";
+            }
             return View();
         }
 
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -5,15 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+            if (_env.IsDevelopment())
+                services.AddScoped<IMailService, DebugMailService>();
+            else
+            {
+
+            }
             services.AddMvc();
         }
 
